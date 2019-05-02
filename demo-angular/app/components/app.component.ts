32a835bbe5f45a59b11e6f21d4e17dc347eedbc0
Daniel Freiling <ddfreiling@gmail.com>
2019-05-03 00:20:50 +0200

    chore: add prefetch test to angular demo

--- demo-angular/app/components/app.component.ts
+++ demo-angular/app/components/app.component.ts
@@ -1,4 +1,9 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as imageModule from 'nativescript-image';
+
+// tslint:disable:no-console
+
+const uri = 'https://www.dccomics.com/sites/default/files/BM_Cv44_A_gallery_5abd2134f11d95.39493991.jpg';
 
 @Component({
     selector: 'my-app',
@@ -36,5 +41,41 @@ export class AppComponent implements OnInit {
             opacity: 1,
             duration: 500
         });
+    }
+
+    prefetchToDisk() {
+        const pipeline = imageModule.getImagePipeline();
+        pipeline.evictFromCache(uri);
+
+        console.time('prefetchToDisk');
+        pipeline
+            .prefetchToDiskCache(uri)
+            .then(() => {
+                console.timeEnd('prefetchToDisk');
+                console.log('in memory cache?', pipeline.isInBitmapMemoryCache(uri));
+                console.log('in disk cache?', pipeline.isInDiskCache(uri));
+            })
+            .catch((err) => {
+                console.log('failed!', err);
+            });
+
+        pipeline.evictFromCache(uri);
+    }
+
+    prefetchToMemory() {
+        const pipeline = imageModule.getImagePipeline();
+        pipeline.evictFromCache(uri);
+
+        console.time('prefetchToMemory');
+        pipeline
+            .prefetchToMemoryCache(uri)
+            .then(() => {
+                console.timeEnd('prefetchToMemory');
+                console.log('in memory cache?', pipeline.isInBitmapMemoryCache(uri));
+                console.log('in disk cache?', pipeline.isInDiskCache(uri));
+            })
+            .catch((err) => {
+                console.log('failed!', err);
+            });
     }
 }
