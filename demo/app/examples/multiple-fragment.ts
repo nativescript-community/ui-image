91eb57936031b71e83f115b14f35d60798efb436
Martin Guillon <martin.guillon@akylas.fr>
2019-06-13 12:13:02 +0200

    test: better demo

--- demo/app/examples/multiple-fragment.ts
+++ demo/app/examples/multiple-fragment.ts
@@ -1,11 +1,11 @@
-import { ListViewEventData, RadListView } from 'nativescript-ui-listview';
+import { CollectionView, CollectionViewItemEventData } from 'nativescript-collectionview';
 import { FinalEventData } from 'nativescript-image';
 import { View } from 'tns-core-modules/ui/page/page';
 
-export function onFinalImageSet(args: FinalEventData) {
-    const listView = (args.object as View).page.getViewById('listView') as RadListView;
-    if (listView && listView.listViewLayout && listView.listViewLayout.ios) {
-        listView.listViewLayout.ios.invalidateLayout();
-    }
+export 
+function onFinalImageSet(args: FinalEventData) {
+    // const listView = (args.object as View).page.getViewById('listView') as CollectionView;
+    // if (listView && listView.listViewLayout && listView.listViewLayout.ios) {
+    //     listView.listViewLayout.ios.invalidateLayout();
+    // }
 }
-
