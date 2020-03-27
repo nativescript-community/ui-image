92ef05c24b1dd784ea32bc4fad5b0a2f037cad35
Martin Guillon <martin.guillon@akylas.fr>
2020-03-27 10:37:15 +0100

    chore: prjecto update

--- src/typings/android.d.ts
+++ src/typings/android.d.ts
@@ -1,2 +1,15 @@
 /// <reference path="./fresco.d.ts" />
 /// <reference path="./fresco-processors.d.ts" />
+
+
+
+
+declare namespace com {
+    export namespace nativescript {
+        export namespace image {
+            class ScalingBlurPostprocessor extends com.facebook.imagepipeline.request.BasePostprocessor {
+                public constructor(iterations:number, blurRadius:number, scaleRatio:number);
+            }
+        }
+    }
+}
