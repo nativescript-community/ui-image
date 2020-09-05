afca0e8a03badde75820185dd04a03b5688e5121
Martin Guillon <martin.guillon@akylas.fr>
2020-09-05 21:35:06 +0200

    fix: angular build fix

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,4 +1,2 @@
 
-/// <reference path="../../node_modules/@nativescript/core/debugger/dom-node.d.ts" />
-/// <reference path="../../node_modules/@nativescript/core/global-types.d.ts" />
 export * from './nativescript-image.module';
