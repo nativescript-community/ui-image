82a6d34fa332eb88faf7ce94b756843b8b5699cc
farfromrefuge <dev@akylas.fr>
2023-05-24 15:05:42 +0200

    chore: project fix

--- src/image-colorfilter/references.d.ts
+++ src/image-colorfilter/references.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../image/references.d.ts" />
 /// <reference path="./typings/android.d.ts" />
+/// <reference path="../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!CoreImage.d.ts" />
