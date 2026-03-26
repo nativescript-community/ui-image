143487bbfd68ff36f7fc692e80877e6730d74d54
farfromrefuge <>
2026-03-26 13:38:18 +0100

    chore: typings

--- src/image/index.d.ts
+++ src/image/index.d.ts
@@ -6,19 +6,19 @@ export * from './index-common';
  * When called, initializes the android Image library. Calling this method is required.
  * A good place to call it is at the application onLaunch() method.
  */
-declare function initialize(config?: ImagePipelineConfigSetting): void;
+export function initialize(config?: ImagePipelineConfigSetting): void;
 
 /**
  * Shuts the native Image and SimpleDraweeView down. By design this method should not be called manually as it is handled by the Image library internally.
  * NOTE: Be careful when manuallycalling this method as it will completely shutdown the functionality of Image.
  */
-declare function shutDown(): void;
+export function shutDown(): void;
 
 /**
  * When called, initializes the android Image library. Calling this method is required.
  * A good place to call it is at the application onLaunch() method.
  */
-declare function getImagePipeline(): ImagePipeline;
+export function getImagePipeline(): ImagePipeline;
 
 /**
  * Encapsulates the common abstraction behind a platform specific object (typically a Bitmap) that is used view to show remote or local images.
